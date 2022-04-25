4fbfb751891c663f52c50a257937352c1e39a451
Stefan Moravcik <stefan@virsas.com>
2022-04-25 19:36:38 +0200

    added region

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_cloudwatch_log_group" "group" {
   name              = var.name
   retention_in_days = var.retention
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+variable "region" { default = "eu-west-1" }
 variable "name" { default = "example" }
 variable "retention" { default = 7 }
 variable "kmskey" { default = "" }
